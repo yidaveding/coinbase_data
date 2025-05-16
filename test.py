226a06b17bd5f75961e6f1d09720e0e638342f62
yidaveding <yi.dave.ding@gmail.com>
2025-05-17 07:19:06 +1000

    modified

--- test.py
+++ test.py
@@ -1,56 +1,24 @@
+import requests
 
-import asyncio, base64, hashlib, hmac, json, os, time, websockets
+# Get product list
+# response = requests.get('https://api.exchange.coinbase.com/products')
+# products = response.json()
+# print(f"Available products: {products}")
 
-API_KEY = str(os.environ.get('API_KEY'))
-PASSPHRASE = str(os.environ.get('PASSPHRASE'))
-SECRET_KEY = str(os.environ.get('SECRET_KEY'))
+# # Get BTC-USD ticker data
+# ticker = requests.get('https://api.exchange.coinbase.com/products/BTC-USD/ticker')
+# ticker_data = ticker.json()
+# print(f"Current BTC price: ${ticker_data['price']}")
 
-URI = 'wss://ws-feed.exchange.coinbase.com'
-SIGNATURE_PATH = '/users/self/verify'
+# Get historical candle data (1-hour intervals)
+# candles = requests.get('https://api.exchange.coinbase.com/products/BTC-USD/candles?granularity=3600')
+candles = requests.get('https://api.exchange.coinbase.com/products/BTC-USD/candles?granularity=86400&start=2024-05-01T00:00:00&end=2024-05-31T23:59:59')
+candle_data = candles.json()
+print(f"Recent candles: {len(candle_data)}")
 
-channel = 'level2'
-product_ids = 'ETH-USD'
-
-
-async def generate_signature():
-    timestamp = str(time.time())
-    message = f'{timestamp}GET{SIGNATURE_PATH}'
-    hmac_key = base64.b64decode(SECRET_KEY)
-    signature = hmac.new(
-        hmac_key,
-        message.encode('utf-8'),
-        digestmod=hashlib.sha256).digest()
-    signature_b64 = base64.b64encode(signature).decode().rstrip('\n')
-    return signature_b64, timestamp
-
-
-async def websocket_listener():
-    signature_b64, timestamp = await generate_signature()
-    subscribe_message = json.dumps({
-        'type': 'subscribe',
-        'channels': [{'name': channel, 'product_ids': [product_ids]}],
-        'signature': signature_b64,
-        'key': API_KEY,
-        'passphrase': PASSPHRASE,
-        'timestamp': timestamp
-    })
-
-    while True:
-        try:
-            async with websockets.connect(URI, ping_interval=None) as websocket:
-                await websocket.send(subscribe_message)
-                while True:
-                    response = await websocket.recv()
-                    json_response = json.loads(response)
-                    print(json_response)
-
-        except (websockets.exceptions.ConnectionClosedError, websockets.exceptions.ConnectionClosedOK):
-            print('Connection closed, retrying..')
-            await asyncio.sleep(1)
-
-
-if __name__ == '__main__':
-    try:
-        asyncio.run(websocket_listener())
-    except KeyboardInterrupt:
-        print("Exiting WebSocket..")
+# 60 = 1 minute candles
+# 300 = 5 minute candles
+# 900 = 15 minute candles
+# 3600 = 1 hour candles
+# 21600 = 6 hour candles
+# 86400 = 1 day candles
